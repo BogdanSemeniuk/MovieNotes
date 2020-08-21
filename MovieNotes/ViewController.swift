c704ea5c96b12d91007a9ba5ec47659a9fca6857
Богдан Семенюк <sbv000@ukr.net>
2020-08-21 12:40:13 +0300

    Initial Commit

---
+++ MovieNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieNotes
+//
+//  Created by Богдан Семенюк on 21.08.2020.
+//  Copyright © 2020 Богдан Семенюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
