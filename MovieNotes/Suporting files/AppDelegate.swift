c69c966d76f2f2f3c855d9563a4ab27c28ab5cbf
Богдан Семенюк <sbv000@ukr.net>
2020-09-10 17:12:06 +0300

    remove unused cod and refactoring

---
+++ MovieNotes/Suporting files/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  MovieNotes
+//
+//  Created by Богдан Семенюк on 21.08.2020.
+//  Copyright © 2020 Богдан Семенюк. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ MovieNotesTests/MovieNotesTests.swift
@@ -0,0 +1,20 @@
+//
+//  MovieNotesTests.swift
+//  MovieNotesTests
+//
+//  Created by Богдан Семенюк on 10.09.2020.
+//  Copyright © 2020 Богдан Семенюк. All rights reserved.
+//
+
+import XCTest
+
+class MovieNotesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
