59c849a8772aa6c27f053576e27f344ca19c3cbb
Богдан Семенюк <sbv000@ukr.net>
2020-09-10 18:13:22 +0300

    add Genre model

---
+++ MovieNotes/Model/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  MovieNotes
+//
+//  Created by Богдан Семенюк on 10.09.2020.
+//  Copyright © 2020 Богдан Семенюк. All rights reserved.
+//
+
+import Foundation
+
+struct Genre: Codable {
+    let id: Int
+    let name: String
+}
--- MovieNotesTests/MovieNotesTests.swift
+++ MovieNotesTests/MovieNotesTests.swift
@@ -11,10 +11,8 @@ import XCTest
 class MovieNotesTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 }
