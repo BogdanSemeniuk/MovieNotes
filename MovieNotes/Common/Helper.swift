35b1395d9c0217702dbe85ee9d9e18202c0d62b9
Богдан Семенюк <sbv000@ukr.net>
2020-09-10 18:16:01 +0300

    add Helper class

---
+++ MovieNotes/Common/Helper.swift
@@ -0,0 +1,13 @@
+//
+//  Helper.swift
+//  MovieNotes
+//
+//  Created by Богдан Семенюк on 10.09.2020.
+//  Copyright © 2020 Богдан Семенюк. All rights reserved.
+//
+
+import Foundation
+
+final class Helper {
+    static let apiKey = "85cfecbf4366c6b833864140f4bc428c"
+}
