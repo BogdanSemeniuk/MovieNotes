9caadbfb26f994cea617e246af1be6555c068b48
Богдан Семенюк <sbv000@ukr.net>
2020-12-18 18:04:56 +0200

    fetchMovies method tests in DataManagerTests

--- MovieNotesTests/Service/DataManagerTests.swift
+++ MovieNotesTests/Service/DataManagerTests.swift
@@ -14,13 +14,14 @@ class DataManagerTests: XCTestCase {
 
     private var sut: DataManager!
     private var storage: UserDefaultsService!
+    private var urlSessionMock: URLSessionMock!
     
     func testDataManagerExistAfterInit() {
         configureSUT(withResponses: [.error])
         XCTAssertNotNil(sut)
     }
     
-    func testDataManagerSaveGenresWhenSuccessInFetchingMovies() {
+    func testSaveGenresWhenSuccessInFetchingMovies() {
         // given
         configureSUT(withResponses: [.genresData, .moviesData])
         let genresExpectation = expectation(description: "Genre expectation")
@@ -41,12 +42,82 @@ class DataManagerTests: XCTestCase {
         }
     }
     
+    func testSaveGenresWhenFailInFetchingMovies() {
+        // given
+        configureSUT(withResponses: [.genresData, .error])
+        let genresExpectation = expectation(description: "Genre expectation")
+        var genre: Genre?
+        // when
+        _ = sut.fetchMovies(page: 1, moviesFilter: .popular).catch { [weak self] _ in
+            genre = self?.storage.fetchGenre(withId: 0)
+            genresExpectation.fulfill()
+        }
+        // then
+        waitForExpectations(timeout: 1) { (_) in
+            XCTAssertNotNil(genre)
+            XCTAssertEqual(genre?.name, "Comedy")
+        }
+    }
+    
+    func testIfGenresWereStoredBeforeGetGenresFromStorageWhenFetchingMovies() {
+        // given
+        configureSUT(withResponses: [.moviesData])
+        let genresList = GenresList(genres: [Genre(id: 0, name: "Action"), Genre(id: 1, name: "Comedy")])
+        let genresExpectation = expectation(description: "Genre expectation")
+        var genre: Genre?
+        // when
+        storage.save(genresList)
+        _ = sut.fetchMovies(page: 1, moviesFilter: .popular).done { [weak self] _ in
+            genre = self?.storage.fetchGenre(withId: 0)
+            genresExpectation.fulfill()
+        }
+        // then
+        waitForExpectations(timeout: 1) { (_) in
+            XCTAssertNotNil(genre)
+            XCTAssertEqual(genre?.name, "Action")
+        }
+    }
+    
+    func testGetErrorWhenFetchigMoviesIfGengesWerentStoredAndGenresRequesFail() {
+        // given
+        configureSUT(withResponses: [.error, .moviesData])
+        let errorExpectation = expectation(description: "Error expectation")
+        var error: Error?
+        // when
+        _ = sut.fetchMovies(page: 1, moviesFilter: .popular).catch { errorResponse in
+            error = errorResponse
+            errorExpectation.fulfill()
+        }
+        // then
+        waitForExpectations(timeout: 1) { (_) in
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error as? MockError, .serverError)
+        }
+    }
+    
+    func testIfGenresWereStoredExecutsOnlyOneRequestWhenFetchingMovies() {
+        // given
+        configureSUT(withResponses: [.moviesData])
+        let genresList = GenresList(genres: [Genre(id: 0, name: "Action"), Genre(id: 1, name: "Comedy")])
+        let requestsCountExpectation = expectation(description: "Request count expectation")
+        // when
+        storage.save(genresList)
+        _ = sut.fetchMovies(page: 1, moviesFilter: .popular).done { _ in
+            requestsCountExpectation.fulfill()
+        }
+        // then
+        waitForExpectations(timeout: 1) { (_) in
+            XCTAssertEqual(self.urlSessionMock.requestsCount, 1)
+        }
+    }
+    
     private func configureSUT(withResponses responses: [ResponseTypeMock]) {
         let userDefaults = UserDefaults(suiteName: #file)!
         userDefaults.removePersistentDomain(forName: #file)
         let coder = Coder.shared
         storage = UserDefaultsService(userDefaults: userDefaults, coder: coder)
+        urlSessionMock = URLSessionMock(responses: responses)
         sut = DataManager(storage: storage,
-                          networking: NetworkingService(session: URLSessionMock(responses: responses), coder: coder))
+                          networking: NetworkingService(session: urlSessionMock, coder: coder))
     }
 }
