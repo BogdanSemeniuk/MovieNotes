f5569f6ec24d580e663966a6d201a71a02e8d092
Богдан Семенюк <>
2021-01-25 18:20:12 +0200

    add ActivitiesRouter and ActivitiesCoordinator

---
+++ MovieNotes/Coordinators/ActivitiesCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  ActivitiesCoordinator.swift
+//  MovieNotes
+//
+//  Created by Богдан Семенюк on 25.01.2021.
+//  Copyright © 2021 Богдан Семенюк. All rights reserved.
+//
+
+import Foundation
+
+final class ActivitiesCoordinator: Coordinator {
+    var children: [Coordinator] = []
+    var router: Router
+    
+    init(router: Router) {
+        self.router = router
+    }
+    
+    func show(animated: Bool, onDismissed: (() -> Void)?) {
+        
+    }
+}
