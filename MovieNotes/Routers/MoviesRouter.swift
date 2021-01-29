0f227c1b1c3fe262a9d13b058fda648b9e874829
Богдан Семенюк <>
2021-01-29 12:47:16 +0200

    ActivitiesRouter and MoviesRouter

--- MovieNotes/Routers/MoviesRouter.swift
+++ MovieNotes/Routers/MoviesRouter.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-final class MoviesRouter: Router {
+final class MoviesRouter: NSObject, Router {
+    let navigationController: UINavigationController
+    private var onDismissForViewController = [UIViewController: (() -> Void)]()
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
     func show(_ viewController: UIViewController, animated: Bool, onDismissed: (() -> Void)?) {
-        
+        onDismissForViewController[viewController] = onDismissed
+        navigationController.pushViewController(viewController, animated: false)
     }
     
     func hide(animated: Bool) {
-        
+        performOnDismissed(for: navigationController.viewControllers.last!)
+        navigationController.popViewController(animated: true)
+    }
+    
+    private func performOnDismissed(for viewController: UIViewController) {
+        guard let onDismiss = onDismissForViewController[viewController] else { return }
+        onDismiss()
+        onDismissForViewController[viewController] = nil
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension MoviesRouter: UINavigationControllerDelegate {
+    public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard let dismissedViewController = navigationController.transitionCoordinator?.viewController(forKey: .from),
+              !navigationController.viewControllers.contains(dismissedViewController) else { return }
+        performOnDismissed(for: dismissedViewController)
     }
 }
